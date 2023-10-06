29a5bcd40a97c672f475802b76ceda01dbd8fda8
ali <ali.saranj@gmail.com>
2023-10-06 11:56:17 -0400

    1

--- api/views.py
+++ api/views.py
@@ -8,13 +8,6 @@ from .PostSerializer import PostSerializer
 # Create your views here.
 @api_view(["GET"])
 def getAllPost(request):
-    try:
-        if("name" in request.GET):
-            print(request.GET["name"])
-        else:
-            print("errr")
-        posts = Post.objects.all()
-        srilaze = PostSerializer(posts, many=True)
-        return Response(srilaze.data)
-    except:
-        return Response({"e"})
+    posts = Post.objects.all()
+    srilaze = PostSerializer(posts, many=True)
+    return Response(srilaze.data)
